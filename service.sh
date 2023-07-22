b419922f851e8abfc2a7e8906f6edc3fd7fe0f5c
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-07-22 21:40:38 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -8,7 +8,7 @@ do
 done
 
 # Sleep an additional 40s to ensure init is finished
-sleep 20
+sleep 40
 
 cp -f /data/data/org.adaway/files/hosts $MODPATH/system/etc/
 cp -f /data_mirror/data_ce/null/0/org.adaway/files/hosts $MODPATH/system/etc/
