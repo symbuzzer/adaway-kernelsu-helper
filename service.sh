c8bc7ab897aed29cc2e6ba37598b5b8a80fd33a3
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-07-22 21:54:57 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,16 +1,13 @@
 #!/system/bin/sh
 
-# Wait for boot to finish completely
 dbg "Sleeping until boot completes."
 while [[ `getprop sys.boot_completed` -ne 1 ]]
 do
        sleep 1
 done
 
-# Sleep an additional 40s to ensure init is finished
-sleep 40
+sleep 1
 
 cp -f /data/data/org.adaway/files/hosts $MODPATH/system/etc/
 cp -f /data_mirror/data_ce/null/0/org.adaway/files/hosts $MODPATH/system/etc/
-ndc resolver flushdefaultif
-dscacheutil -flushcache
+
