fa2036e7cb90462892a5a2afcbd315eeeb388570
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-08-16 23:51:26 +0300

    Create boot-completed.sh

---
+++ boot-completed.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+dbg "Sleeping until boot completes."
+while [[ `getprop sys.boot_completed` -ne 1 ]]
+do
+       sleep 1
+done
+
+sleep 40
+
+cp -f /data/data/org.adaway/files/hosts $MODPATH/system/etc/
+cp -f /data_mirror/data_ce/null/0/org.adaway/files/hosts $MODPATH/system/etc/
